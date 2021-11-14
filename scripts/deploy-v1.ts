80d03064aaa50623ed4520b1c2b96393f5603d67
tunogya <tunogya@qq.com>
2021-11-14 22:28:37 +0800

    add deployed

--- scripts/deploy-v1.ts
+++ scripts/deploy-v1.ts
@@ -12,6 +12,11 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  // 2021-11-14 22:29, v1 deploy result
+  // DuneAvatar deployed to: 0x47F172D5f8ff7ffeED6aA1Dc34a35b1d07Ea612d
+  // DuneItems deployed to: 0x9dEF0e1A4499B100E1A4C31Fab01A1358dB44AAA
+  // DuneLand deployed to: 0x80848483e0dC9f9bCe8711465bc7fd21c5d8e573
+  // DuneSpices deployed to: 0x151ECe9ed0b928cD49332FD0FAb20341B3E641d5
 
   // We get the contract to deploy
   const DuneAvatar = await ethers.getContractFactory("DuneAvatar");
