dc1408955a7d7de52b9705ed104685e4c44b843e
tunogya <tunogya@qq.com>
2021-11-17 18:57:41 +0800

    Revert "delete Ownable"
    This reverts commit 8ec51336e294a3206eaf9ce3a1366caf42d49c3c.

--- contracts/DuneLand.sol
+++ contracts/DuneLand.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 /// @custom:security-contact tunogya@qq.com
-contract DuneLand is ERC721, ERC721Enumerable {
+contract DuneLand is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("DuneLand", "LAND") {}
 
-    function safeMint(address to) public {
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
--- contracts/DuneSpices.sol
+++ contracts/DuneSpices.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DuneSpices is ERC20, ERC20Burnable {
-    constructor() ERC20("DuneSpices", "SPICES") {}
+/// @custom:security-contact tunogya@qq.com
+contract DuneSpices is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    function mint(address to, uint256 amount) public {
+    constructor() ERC20("DuneSpices", "SPICES") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 }
